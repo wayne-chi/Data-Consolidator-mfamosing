59a4437558bd99891eb35a737f66bda06a271380
wayne-chi <56788664+wayne-chi@users.noreply.github.com>
2026-02-24 17:57:34 +0100

    corrected a little error with st.tables

--- app.py
+++ app.py
@@ -182,7 +182,7 @@ if uploaded_files:
                 if not final_report.empty:
                     st.success("Consolidation complete!")
                     # st.dataframe(final_report.head())
-                    st.table(st.table(final_report.style.hide(axis="index")))
+                    st.table(final_report.style.hide(axis="index"))
                     
                     # 4. Create Downloadable Excel
                     buffer = io.BytesIO()
@@ -204,5 +204,6 @@ if uploaded_files:
             else:
 
                 st.error("No valid files matching the criteria (mfamosing + genset keywords) were found.")
+
 
 
